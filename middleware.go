c8b7df88255dac0d137076eb5b2739de0a911ee6
Semir Mahovkic <semirm.dev@gmail.com>
2022-05-29 22:46:16 +0200

    hamr

--- middleware.go
+++ middleware.go
@@ -14,12 +14,7 @@ import (
 // authorize middleware will check if request is authorized.
 // If adapter is passed Casbin policy will be checked as well
 // TODO: removing Gin dependency in progress
-func (auth *auth) authorize(
-	obj, act string,
-	adapter *gormadapter.Adapter,
-	w http.ResponseWriter,
-	r *http.Request) (bool, error) {
-
+func (auth *auth) authorize(obj, act string, adapter *gormadapter.Adapter, w http.ResponseWriter, r *http.Request) (bool, error) {
 	_, token := getAccessTokenFromRequest(w, r)
 	if strings.TrimSpace(token) == "" {
 		return false, errors.New("token not found")
